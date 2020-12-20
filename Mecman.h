627e1db88b5544903667de0b32b9b6d1fc40acdd
Daniel <danielmv829@gmail.com>
2020-12-20 17:44:23 -0300

    Mecman.h updated

--- Mecman.h
+++ Mecman.h
@@ -4,6 +4,9 @@
 #include "Character.h"
 #include <ncurses.h> /* Biblioteca para o sistema de i/o do jogo. 
 					    Documentação em https://tldp.org/HOWTO/NCURSES-Programming-HOWTO/ */
+#ifndef MECMAN_H
+#define MECMAN_H
+
 
 class Mecman: public Character
 {
@@ -18,3 +21,5 @@ class Mecman: public Character
         bool isAlive();
     
 };
+
+#endif
